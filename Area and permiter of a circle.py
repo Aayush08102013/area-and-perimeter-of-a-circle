efcb2f484254779b8bbe1bc577313c372877a67c
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-07-01 19:03:00 +1200

    Area and perimeter of a circle.py

--- Area and permiter of a circle.py
+++ Area and permiter of a circle.py
@@ -16,24 +16,5 @@ circle = Circle(radius_input)
 print("Area of the circle:", circle.area(),
 "Perimeter of the circle:", circle.perimeter())
 
-import math
-
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius ** 2
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
-
-# Taking input from the user
-radius_input = float(input("Enter the radius of the circle: "))
-circle = Circle(radius_input)
-
-print("Area of the circle:", circle.area())
-print("Perimeter of the circle:", circle.perimeter())
-
 
     
